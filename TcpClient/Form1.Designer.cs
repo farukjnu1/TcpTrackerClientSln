9c39ece786b3352f5ce38de71f4c14096580bc14
farukjnu1@gmail.com <farukjnu1@gmail.com>
2025-09-09 03:03:11 +0800

    Add project files.

---
+++ TcpClient/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace TcpTrackerClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SendMsgButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.serveripTextbox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.userNameTextbox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.totextbox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.messagebodytextbox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.portTextbox = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // SendMsgButton
+            // 
+            this.SendMsgButton.Location = new System.Drawing.Point(276, 168);
+            this.SendMsgButton.Name = "SendMsgButton";
+            this.SendMsgButton.Size = new System.Drawing.Size(75, 23);
+            this.SendMsgButton.TabIndex = 0;
+            this.SendMsgButton.Text = "Send";
+            this.SendMsgButton.UseVisualStyleBackColor = true;
+            this.SendMsgButton.Click += new System.EventHandler(this.SendMsgButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "UserName";
+            // 
+            // serveripTextbox
+            // 
+            this.serveripTextbox.Location = new System.Drawing.Point(76, 16);
+            this.serveripTextbox.Name = "serveripTextbox";
+            this.serveripTextbox.Size = new System.Drawing.Size(177, 20);
+            this.serveripTextbox.TabIndex = 2;
+            this.serveripTextbox.Text = "103.10.55.187";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Server IP";
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(263, 52);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(75, 23);
+            this.ConnectButton.TabIndex = 4;
+            this.ConnectButton.Text = "Connect";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // userNameTextbox
+            // 
+            this.userNameTextbox.Location = new System.Drawing.Point(76, 52);
+            this.userNameTextbox.Name = "userNameTextbox";
+            this.userNameTextbox.Size = new System.Drawing.Size(177, 20);
+            this.userNameTextbox.TabIndex = 5;
+            this.userNameTextbox.Text = "server";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "To :";
+            // 
+            // totextbox
+            // 
+            this.totextbox.Location = new System.Drawing.Point(68, 24);
+            this.totextbox.Name = "totextbox";
+            this.totextbox.Size = new System.Drawing.Size(284, 20);
+            this.totextbox.TabIndex = 7;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.messagebodytextbox);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.SendMsgButton);
+            this.groupBox1.Controls.Add(this.totextbox);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(10, 98);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(381, 202);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "ChatBox";
+            // 
+            // messagebodytextbox
+            // 
+            this.messagebodytextbox.Location = new System.Drawing.Point(68, 52);
+            this.messagebodytextbox.Multiline = true;
+            this.messagebodytextbox.Name = "messagebodytextbox";
+            this.messagebodytextbox.Size = new System.Drawing.Size(283, 108);
+            this.messagebodytextbox.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 55);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Message :";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 328);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(380, 121);
+            this.listBox1.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 308);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Message Log";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(262, 19);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Server Port";
+            // 
+            // portTextbox
+            // 
+            this.portTextbox.Location = new System.Drawing.Point(328, 16);
+            this.portTextbox.Name = "portTextbox";
+            this.portTextbox.Size = new System.Drawing.Size(68, 20);
+            this.portTextbox.TabIndex = 12;
+            this.portTextbox.Text = "1234";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 460);
+            this.Controls.Add(this.portTextbox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.userNameTextbox);
+            this.Controls.Add(this.ConnectButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.serveripTextbox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Tcp Tracker Client";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SendMsgButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox serveripTextbox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.TextBox userNameTextbox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox totextbox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox messagebodytextbox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox portTextbox;
+    }
+}
+
---
+++ TcpClient/Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using System.Data.SqlClient;
+
+namespace TcpTrackerClient
+{
+    public partial class Form1 : Form
+    {
+        TcpClient tcpClient;
+        bool isConnectedToServer = false;
+        private StreamWriter strWritter;
+        private StreamReader strReader;
+        private Thread incomingMessageHandler;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        void getResponse()
+        {
+            Stream stm = tcpClient.GetStream();
+            byte[] bb = new byte[100];
+            int k = stm.Read(bb, 0, 100);
+
+            string msg = "";
+
+            for (int i = 0; i < k; i++)
+            {
+                msg += Convert.ToChar(bb[i]).ToString();
+
+            }
+
+            setClientMessage(msg);
+        }
+
+
+        void setClientMessage(string msg)
+        {
+
+            if (!InvokeRequired)
+            {
+                listBox1.Items.Add(msg.ToString());
+
+
+            }
+            else
+            {
+                Invoke(new Action<string>(setClientMessage), msg);
+            }
+        }
+
+        private void SendMsgButton_Click(object sender, EventArgs e)
+        {
+            string message;
+            message = "SDx;" + totextbox.Text + ";" + messagebodytextbox.Text.Trim();
+            strWritter.WriteLine(message);
+            strWritter.Flush();
+
+        }
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            if (!isConnectedToServer)
+            {
+                string connectionEstablish;
+
+                connectionEstablish = "ATx;" + userNameTextbox.Text.Trim();
+
+                tcpClient = new TcpClient();
+                tcpClient.Connect(serveripTextbox.Text.Trim(), int.Parse(portTextbox.Text.Trim()));
+                this.isConnectedToServer = true;
+
+                strWritter = new StreamWriter(tcpClient.GetStream());
+                strWritter.WriteLine(connectionEstablish);
+                strWritter.Flush();
+
+                incomingMessageHandler = new Thread(() => ReceiveMessages());
+                incomingMessageHandler.IsBackground = true;
+                incomingMessageHandler.Start();
+
+                ConnectButton.Text = "Connected";
+            }
+            else
+            {
+                MessageBox.Show("Already connected");
+            }
+
+        }
+
+        
+        private void ReceiveMessages()
+        {
+            
+
+            strReader = new StreamReader(this.tcpClient.GetStream());
+
+            // While we are successfully connected, read incoming lines from the server
+            while (this.isConnectedToServer)
+            {
+                string serverResponse = strReader.ReadLine();
+                string[] data = serverResponse.Split(';');
+
+                if (data[0].Equals("RDx"))
+                {
+                    string source = data[1];
+                    string message = data[2];
+
+                    setClientMessage(source + " Says:" + " " + message);
+                    if (message.LastIndexOf("FF") > 0 && message.Contains(','))
+                    {
+                        // save in database
+                        if (message.Split(',').Count() == 11)
+                        {
+                            SqlConnection con;
+                            SqlCommand cmd;
+                            string conStr = "Data Source=localhost;Initial Catalog=TwoStepDb;User ID=sa;Password=123";
+
+                            // delete
+                            con = new SqlConnection(conStr);
+                            cmd = new SqlCommand();
+                            cmd.Connection = con;
+                            con.Open();
+                            cmd.CommandText = "DELETE FROM [dbo].[Tbl_TrackLast] WHERE Tid = @Tid";
+                            cmd.Parameters.Add(new SqlParameter("Tid", source));
+                            cmd.ExecuteNonQuery();
+                            cmd.Dispose();
+                            con.Close();
+
+                            //insert
+                            con = new SqlConnection(conStr);
+                            cmd = new SqlCommand();
+                            cmd.Connection = con;
+                            con.Open();
+                            cmd.CommandText = "INSERT INTO [dbo].[Tbl_TrackLast] VALUES(@Tid, @Lat, @Lon, @Direction, @GpsSignal, @Speed, @Temperature, @GsmSignal, @ReceiveTime, @CarState, @TeState, @AlarmState, @Live)";
+                            cmd.Parameters.Add(new SqlParameter("Tid", source));
+                            cmd.Parameters.Add(new SqlParameter("Lat", message.Split(',')[0]));
+                            cmd.Parameters.Add(new SqlParameter("Lon", message.Split(',')[1]));
+                            cmd.Parameters.Add(new SqlParameter("Direction", message.Split(',')[2]));
+                            cmd.Parameters.Add(new SqlParameter("GpsSignal", message.Split(',')[3]));
+                            cmd.Parameters.Add(new SqlParameter("Speed", message.Split(',')[4]));
+                            cmd.Parameters.Add(new SqlParameter("Temperature", message.Split(',')[5]));
+                            cmd.Parameters.Add(new SqlParameter("GsmSignal", message.Split(',')[6]));
+                            cmd.Parameters.Add(new SqlParameter("ReceiveTime", DateTime.Now));
+                            cmd.Parameters.Add(new SqlParameter("CarState", message.Split(',')[7]));
+                            cmd.Parameters.Add(new SqlParameter("TeState", message.Split(',')[8]));
+                            cmd.Parameters.Add(new SqlParameter("AlarmState", message.Split(',')[9]));
+                            cmd.Parameters.Add(new SqlParameter("Live", true));
+                            cmd.ExecuteNonQuery();
+                            cmd.Dispose();
+                            con.Close();
+
+                            //insert
+                            if (source.Contains('-'))
+                            {
+                                source = "Track__" + source;
+                            }
+                            else
+                            {
+                                source = "Track_" + source;
+                            }
+
+                            //select
+                            double lat = 0;
+                            double lon = 0;
+                            int calMileage = 0; 
+                            int prevMilage = 0;
+                            SqlDataReader rd;
+                            con = new SqlConnection(conStr);
+                            cmd = new SqlCommand();
+                            cmd.Connection = con;
+                            con.Open();
+                            cmd.CommandText = "SELECT TOP 1 Id, Lat, Lon, Mileage FROM " + source + " ORDER BY Id DESC";
+                            rd = cmd.ExecuteReader();
+                            rd.Read();
+                            if (rd.HasRows)
+                            {
+                                lat = Convert.ToDouble(rd.GetValue(1));
+                                lon = Convert.ToDouble(rd.GetValue(2));
+                                prevMilage = rd.GetInt32(3);
+                            }
+                            cmd.Dispose();
+                            rd.Close();
+                            con.Close();
+
+                            calMileage = (int)DirectDistance(lat, lon, Convert.ToDouble(message.Split(',')[0]), Convert.ToDouble(message.Split(',')[1]));
+                            if (calMileage > -1)
+                            {
+                                calMileage = calMileage + prevMilage;
+
+                                con = new SqlConnection(conStr);
+                                cmd = new SqlCommand();
+                                cmd.Connection = con;
+                                con.Open();
+                                cmd.CommandText = "INSERT INTO " + source + " VALUES (@Lat, @Lon, @Direction, @GpsSignal, @Speed, @Temperature, @GsmSignal, @ReceiveTime, @CarState, @TeState, @AlarmState, @Mileage);";
+                                cmd.Parameters.Add(new SqlParameter("Lat", message.Split(',')[0]));
+                                cmd.Parameters.Add(new SqlParameter("Lon", message.Split(',')[1]));
+                                cmd.Parameters.Add(new SqlParameter("Direction", message.Split(',')[2]));
+                                cmd.Parameters.Add(new SqlParameter("GpsSignal", message.Split(',')[3]));
+                                cmd.Parameters.Add(new SqlParameter("Speed", message.Split(',')[4]));
+                                cmd.Parameters.Add(new SqlParameter("Temperature", message.Split(',')[5]));
+                                cmd.Parameters.Add(new SqlParameter("GsmSignal", message.Split(',')[6]));
+                                cmd.Parameters.Add(new SqlParameter("ReceiveTime", DateTime.Now));
+                                cmd.Parameters.Add(new SqlParameter("CarState", message.Split(',')[7]));
+                                cmd.Parameters.Add(new SqlParameter("TeState", message.Split(',')[8]));
+                                cmd.Parameters.Add(new SqlParameter("AlarmState", message.Split(',')[9]));
+                                cmd.Parameters.Add(new SqlParameter("Mileage", calMileage));
+                                cmd.ExecuteNonQuery();
+                                cmd.Dispose();
+                                con.Close();
+                            }
+                            
+                        }
+                    }
+                }
+
+            }
+        }
+
+        double DirectDistance(double lat1, double lng1, double lat2, double lng2)
+        {
+            double earthRadius = 3959.00;
+            double dLat = ToRadians(lat2 - lat1);
+            double dLng = ToRadians(lng2 - lng1);
+            double a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
+                       Math.Cos(ToRadians(lat1)) * Math.Cos(ToRadians(lat2)) *
+                       Math.Sin(dLng / 2) * Math.Sin(dLng / 2);
+            double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+            double dist = earthRadius * c;
+            double meterConversion = 1609.334;
+            return Math.Ceiling(dist * meterConversion);
+        }
+
+        double ToRadians(double degrees)
+        {
+            double radians = degrees * 3.14159265 / 180;
+            return radians;
+        }
+
+    }
+}
+
+//ATx;id;
+//SDx;remote id;msg
+//RDx;remote id;msg
+
+
